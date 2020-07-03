3339b7db72afe982a6fb424f8e480ef393c6019f
Chiva Chen <q932104843@outlook.com>
2020-07-03 21:18:58 +0800

    添加内存缓存

---
+++ src/Mango.Core/Cache/MangoMemoryCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mango.Core.Cache
+{
+    /// <summary>
+    /// 内存缓存
+    /// </summary>
+    public class MangoMemoryCache
+    {
+    }
+}
