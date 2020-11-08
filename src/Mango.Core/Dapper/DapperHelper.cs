f66ba11040db806ab6aa9417d2daf462e74ee002
Chiva Chen <q932104843@outlook.com>
2020-11-08 09:20:41 +0800

    增加Dapper支持

---
+++ src/Mango.Core/Dapper/DapperHelper.cs
@@ -0,0 +1,116 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Core.Dapper
+{
+    /// <summary>
+    /// Dapper帮助类
+    /// </summary>
+    public class DapperHelper
+    {
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// 使用连接字符串初始化
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public DapperHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Query
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public IEnumerable<T> Query<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.Query<T>(sql, param);
+            }
+        }
+
+        /// <summary>
+        /// QueryFirst
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public T QueryFirst<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.QueryFirst<T>(sql, param);
+            }
+        }
+
+        /// <summary>
+        /// QueryFirstOrDefault
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public T QueryFirstOrDefault<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.QueryFirstOrDefault<T>(sql, param);
+            }
+        }
+
+        /// <summary>
+        /// QueryAsync
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.QueryAsync<T>(sql, param);
+            }
+        }
+
+        /// <summary>
+        /// QueryFirstAsync
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public Task<T> QueryFirstAsync<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.QueryFirstAsync<T>(sql, param);
+            }
+        }
+
+        /// <summary>
+        /// QueryFirstOrDefaultAsync
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                return cn.QueryFirstOrDefaultAsync<T>(sql, param);
+            }
+        }
+    }
+}
---
+++ src/Mango.Core/Dapper/Extension/DapperExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mango.Core.Dapper.Extension
+{
+    /// <summary>
+    /// Dapper扩展类
+    /// </summary>
+    public static class DapperExtension
+    {
+        /// <summary>
+        /// 添加Dapper支持
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString">连接字符串</param>
+        /// <returns></returns>
+        public static IServiceCollection AddDapper(this IServiceCollection services, string connectionString)
+        {
+            var dh = new DapperHelper(connectionString);
+            services.AddSingleton(dh);
+            return services;
+        }
+    }
+}
